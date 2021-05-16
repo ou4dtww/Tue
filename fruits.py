24c0ddb0224125936624507cea0f6bf4d3b003aa
ou4dtww <34579654+ou4dtww@users.noreply.github.com>
2021-05-16 21:07:56 +0100

    Updated add by fruit

--- fruits.py
+++ fruits.py
@@ -22,6 +22,31 @@ if f.mode == 'r':
 
 # Give total quantity
 print("Total Fruits:",word_count(contents))
-    
+
+# Output as a set occurence of word in string 'contents'
+# Use / build up a dictionary of plural words, replace plural 
+# as singular, and count them as single occurence
+# e.g. each 'apples' is changed to 'apple' and counted as 1
+
+# Create a set type, split the words, and change data type of contents
+set1=set()
+contents=contents.split(" ")
+a=0
+
+# see explanation above
+for x in contents:
+    a=a+1
+    if x == 'apples':
+        contents[a] = 'apple'
+
+# reset counter
+a=0
+
+# build up the set 
+for i in contents:
+    set1.add((i,contents.count(i)))
+
+print("Total fruits and quantity for each:", set1)
+
 # close the file 
 f.close()
